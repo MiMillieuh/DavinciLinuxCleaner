2934eb6d7605c19ca7d362bc45cf7ecab41bbba3
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-02-01 20:58:20 +0100

    Add files via upload

---
+++ DavinciCleaner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f /usr/share/applications/com.blackmagicdesign.rawplayer.desktop
+rm -f /usr/share/applications/com.blackmagicdesign.rawspeedtest.desktop
+rm -f /usr/share/applications/com.blackmagicdesign.resolve-CaptureLogs.desktop
+rm -f /usr/share/applications/com.blackmagicdesign.resolve-DaVinciRemoteMonitoring.desktop
+rm -f /usr/share/applications/com.blackmagicdesign.resolve-Installer.desktop
+rm -f /usr/share/applications/com.blackmagicdesign.resolve-Panels.desktop
+
+ echo 'PrefersNonDefaultGPU=true' >> /usr/share/applications/com.blackmagicdesign.resolve.desktop
+ echo 'X-KDE-RunOnDiscreteGpu=true' >> /usr/share/applications/com.blackmagicdesign.resolve.desktop
